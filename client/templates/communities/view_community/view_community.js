cbf901072ff9fee4a609e5a8d435627de8516904
Fabien Saulier <fsaulier@hotmail.com>
2016-02-10 20:13:45 +0000

    template community

---
+++ client/templates/communities/view_community/view_community.js
@@ -0,0 +1,26 @@
+/*****************************************************************************/
+/* ViewCommunity: Event Handlers */
+/*****************************************************************************/
+Template.ViewCommunity.events({
+});
+
+/*****************************************************************************/
+/* ViewCommunity: Helpers */
+/*****************************************************************************/
+Template.ViewCommunity.helpers({
+
+});
+
+
+
+/*****************************************************************************/
+/* ViewCommunity: Lifecycle Hooks */
+/*****************************************************************************/
+Template.ViewCommunity.onCreated(function () {
+});
+
+Template.ViewCommunity.onRendered(function () {
+});
+
+Template.ViewCommunity.onDestroyed(function () {
+});
